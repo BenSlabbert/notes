8952bcd979a60e74f05f771bd19b2125968b9830
Ben Slabbert <ben@hex.co.za>
2017-10-04 14:14:29 +0200

    adding text area and header adding meteor call as prop

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -2,16 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {createContainer} from 'meteor/react-meteor-data';
 import {Session} from 'meteor/session';
+import {Meteor} from 'meteor/meteor';
 
 import {Notes} from '../api/notes';
 
 export class Editor extends React.Component {
 
+    handleBodyChange(e) {
+
+        this.props.call('notes.update', this.props.note._id, {
+            body: e.target.value
+        });
+    }
+
+    handleTitleChange(e) {
+
+        this.props.call('notes.update', this.props.note._id, {
+            title: e.target.value
+        });
+    }
+
     render() {
 
         if (this.props.note) { // we have a note
             return (
-                <p>Note found</p>
+                <p>
+                    <input value={this.props.note.title}
+                           placeholder="Untitled"
+                           onChange={this.handleTitleChange.bind(this)}/>
+                    <textarea value={this.props.note.body}
+                              placeholder="Note here ..."
+                              onChange={this.handleBodyChange.bind(this)}>
+                    </textarea>
+                    <button>Delete Note</button>
+                </p>
             );
         } else { // nothing selected
             return (
@@ -34,6 +58,7 @@ export default createContainer(() => {
 
     return {
         selectedNoteId,
-        note: Notes.findOne(selectedNoteId)
+        note: Notes.findOne(selectedNoteId),
+        call: Meteor.call
     };
 }, Editor);
