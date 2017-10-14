4f5addd48967e5d91fb61bfdbb79421fb56571ea
Ben Slabbert <ben@hex.co.za>
2017-10-14 11:57:34 +0200

    finishing mobile styles

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Accounts } from 'meteor/accounts-base';
-import { createContainer } from 'meteor/react-meteor-data';
+import {Accounts} from 'meteor/accounts-base';
+import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
 
 export const PrivateHeader = (props) => {
+
+    const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
+
     return (
         <div className="header">
             <div className="header__content">
+                <img className="header__nav-toggle" onClick={props.handleNavToggle} src={navImageSrc}/>
                 <h1 className="header__title">{props.title}</h1>
                 <button className="button button--link-text" onClick={() => {
                     props.handleLogout()
-                }}>Logout</button>
+                }}>Logout
+                </button>
             </div>
         </div>
     );
@@ -18,15 +24,19 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
     title: PropTypes.string.isRequired,
-    handleLogout: PropTypes.func.isRequired
+    handleLogout: PropTypes.func.isRequired,
+    handleNavToggle: PropTypes.func.isRequired,
+    isNavOpen: PropTypes.bool.isRequired
 };
 
-// anything returnd from this function is a prop
+// anything returned from this function is a prop
 // to the presentational component above
 export default createContainer(() => {
     return {
-        handleLogout: () => { Accounts.logout(); }
+        handleLogout: () => {
+            Accounts.logout();
+        },
+        isNavOpen: Session.get('isNavOpen'),
+        handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen'))
     };
 }, PrivateHeader);
-
-// export default PrivateHeader;
